2aa0722bc477e9e08a2b50258bac6d21a133bc18
Estephan Rustom <steverustom@gmail.com>
2019-05-02 15:32:39 +0300

    mutableProperty

--- ReactiveBeacons/BeaconManager.swift
+++ ReactiveBeacons/BeaconManager.swift
@@ -6,53 +6,73 @@
 //  Copyright © 2019 Steve Rustom. All rights reserved.
 //
 
-//import Foundation
-//import CoreLocation
-//import ReactiveCocoa
-//import ReactiveSwift
-//import Result
-//
-//class BeaconManager: NSObject, CLLocationManagerDelegate {
-//   
-//    var disposable: Disposable?
-//    let locationManager : CLLocationManager? = nil
-//    
-////    init(locationManager: CLLocationManager) {
-////        self.locationManager = locationManager
-////        requestAuthorization()
-////    }
-////    
-////    func requestAuthorization() {
-////        locationManager!.delegate = self
-////        locationManager!.allowsBackgroundLocationUpdates = true
-////        if CLLocationManager.authorizationStatus() != .authorizedAlways {
-////            locationManager!.requestAlwaysAuthorization()
-////        }
-////    }
-////    
-////    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-////        switch status {
-////        case .authorizedAlways:
-////            print("Authorization always granted.")
-////        case .authorizedWhenInUse:
-////            print("Authorization granted when in use.")
-////        default:
-////            print("Authorization not granted.")
-////        }
-////    }
-//    
-//    let sp = SignalProducer<[CLBeacon], NoError> { observer, lifetime in
-//        let beaconsArray : [CLBeacon] = []
-//        
-//        print("Start Monitoring")
-//        let region = CLBeaconRegion(proximityUUID: UUID(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!, major: 50075, minor: 56949, identifier: "Estimote.")
-//        region.notifyOnEntry = true
-//        region.notifyOnExit = true
-//        region.notifyEntryStateOnDisplay = true
-////        locationManager.startRangingBeaconsInRegion(region)
-////        locationManager.startMonitoring(for: region)
-////        locationManager.startUpdatingLocation()
-////        locationManager.startRangingBeacons(in: region)
-//    }
-//    
-//}
+import Foundation
+import CoreLocation
+import ReactiveCocoa
+import ReactiveSwift
+import Result
+
+class BeaconManager: NSObject, CLLocationManagerDelegate {
+   
+    let majorValue : CLBeaconMajorValue
+    let minorValue : CLBeaconMinorValue
+    let uuid : UUID
+    let name : String
+    let locationManager = CLLocationManager()
+    
+    var property: MutableProperty<[CLBeacon]> = MutableProperty([])
+    
+    init(name: String, uuid: UUID, majorValue: Int, minorValue: Int) {
+        self.name = name
+        self.uuid = uuid
+        self.majorValue = CLBeaconMajorValue(majorValue)
+        self.minorValue = CLBeaconMinorValue(minorValue)
+    }
+
+    //MARK: - CoreLocation
+    
+    func requestAuthorization() {
+        locationManager.delegate = self
+        locationManager.allowsBackgroundLocationUpdates = true
+        if CLLocationManager.authorizationStatus() != .authorizedAlways {
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedAlways:
+            print("Authorization always granted.")
+        case .authorizedWhenInUse:
+            print("Authorization granted when in use.")
+        default:
+            print("Authorization not granted.")
+        }
+    }
+    
+    private func asBeaconRegion() -> CLBeaconRegion {
+        let region = CLBeaconRegion(proximityUUID: uuid, major: majorValue, minor: minorValue, identifier: name)
+        region.notifyOnEntry = true
+        region.notifyOnExit = true
+        return region
+    }
+    
+    func startMonitoring() {
+        print("Start Monitoring")
+        let beaconRegion = self.asBeaconRegion()
+        beaconRegion.notifyEntryStateOnDisplay = true
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startUpdatingLocation()
+        locationManager.startRangingBeacons(in: beaconRegion)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if beacons.count > 0 {
+            for beacon in beacons {
+                if(!property.value.contains(beacon)) {
+                    property.value.append(beacon)
+                }
+            }
+        }
+    }
+}
--- ReactiveBeacons/ViewController.swift
+++ ReactiveBeacons/ViewController.swift
@@ -8,135 +8,41 @@
 
 import UIKit
 import CoreLocation
-
 import ReactiveCocoa
 import ReactiveSwift
 import Result
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController {
     
-    let locationManager = CLLocationManager()
-    var beaconsArray : [CLBeacon] = []
-    
-    var disposable: Disposable?
+     let beaconManager = BeaconManager(name: "Estimote", uuid: UUID(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")! ,majorValue: 50075, minorValue: 56949)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-//        requestAuthorization()
-        
-        let observer = Signal<[CLBeacon], NoError>.Observer( value: { value in
-            var beaconUUIDs = [UUID]()
-            for beacon in value {
-                beaconUUIDs.append(beacon.proximityUUID)
-            }
-
-            print("UUID: \(beaconUUIDs)")
-        })
+    
+        self.beaconManager.requestAuthorization()
+        self.beaconManager.startMonitoring()
 
         let sp = SignalProducer<[CLBeacon], NoError> { observer, lifetime in
             
-//            self.requestAuthorization()
-
-//            self.startMonitoring()
-            DispatchQueue.main.async(execute: {
-                self.requestAuthorization()
-                self.startMonitoring()
-//                observer.send(value: self.beaconsArray)
-//                observer.sendCompleted()
-            })
-            observer.send(value: self.beaconsArray)
+            guard !lifetime.hasEnded else {
+                observer.sendInterrupted()
+                return
+            }
+            
+            observer.send(value: self.beaconManager.property.value)
             observer.sendCompleted()
         }
-
-        disposable = sp.start(observer)
-    }
-    
-    //MARK: - CoreLocation
-    
-    func requestAuthorization() {
-        locationManager.delegate = self
-        locationManager.allowsBackgroundLocationUpdates = true
-        if CLLocationManager.authorizationStatus() != .authorizedAlways {
-            locationManager.requestAlwaysAuthorization()
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        switch status {
-        case .authorizedAlways:
-            print("Authorization always granted.")
-        case .authorizedWhenInUse:
-            print("Authorization granted when in use.")
-        default:
-            print("Authorization not granted.")
-        }
-    }
-    
-     private func asBeaconRegion() -> CLBeaconRegion {
-        let region = CLBeaconRegion(proximityUUID: UUID(uuidString: "B9407F30-F5F8-466E-AFF9-25556B57FE6D")!, major: 50075, minor: 56949, identifier: "Estimote.")
-        region.notifyOnEntry = true
-        region.notifyOnExit = true
-        return region
-    }
-    
-    func startMonitoring() {
-        print("Start Monitoring")
-        let beaconRegion = self.asBeaconRegion()
-        beaconRegion.notifyEntryStateOnDisplay = true
-        locationManager.startMonitoring(for: beaconRegion)
-        locationManager.startUpdatingLocation()
-        locationManager.startRangingBeacons(in: beaconRegion)
-    }
-    
-//    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
-//        print("Did enter.")
-//        UserDefaults.standard.set("0", forKey: "steve")
-//        var steve = 0
-//        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
-//            steve += 1
-//            UserDefaults.standard.set(String(steve), forKey: "steve")
-//            UserDefaults.standard.synchronize()
-//            print(steve)
-//        }
-//        
-//    }
-//    
-//    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
-//        print("Did exit.")
-//    }
-    
-    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
-        if beacons.count > 0 {
-            updateDistance(beacons[0].proximity)
-            
-            for beacon in beacons {
-                beaconsArray.append(beacon)
-            }
-        } else {
-            updateDistance(.unknown)
-        }
-    }
-    
-    private func updateDistance(_ distance: CLProximity) {
-        UIView.animate(withDuration: 0.8) {
-            switch distance {
-            case .unknown:
-                self.view.backgroundColor = UIColor.gray
-                
-            case .far:
-                self.view.backgroundColor = UIColor.red
-                
-            case .near:
-                self.view.backgroundColor = UIColor.orange
-                
-            case .immediate:
-                self.view.backgroundColor = UIColor.green
-
-            @unknown default:
-                fatalError()
+        
+        let property = beaconManager.property
+        
+        property.producer.startWithValues { (value) in
+            for beacon in value {
+                print("UUID: \(beacon.proximityUUID), RSSI: \(beacon.rssi)")
             }
         }
+
+        property <~ sp
     }
 }
 
